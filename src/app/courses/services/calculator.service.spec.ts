e8eac5113a33118402f8079ae1e64874a3511108
devLucasOAK <devlucasoak@outook.com>
2021-09-05 20:04:25 -0300

    change to firefox/SpyOn

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -3,14 +3,17 @@ import { LoggerService } from "./logger.service";
 
 describe('CalculatorService', ()=> {
     
-    it('should add two numbers', ()=>{      
+    it('should add two numbers', ()=>{
 
-        const calculator = new CalculatorService(new LoggerService);
+        //Spy
+        const logger = jasmine.createSpyObj('logger', ['log']);
+
+        const calculator = new CalculatorService(logger);
         const result = calculator.add(2, 2);
 
+        expect(logger.log).toHaveBeenCalledTimes(1);
         expect(result).toBe(4);
 
-        
     })
 
     it('should substract two numbers', ()=>{
