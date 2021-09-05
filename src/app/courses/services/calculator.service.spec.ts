69a136d239c3efd5e3a9592e4231ef317d7769bf
devLucasOAK <devlucasoak@outook.com>
2021-09-05 20:18:25 -0300

    BeforeEach

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,27 +1,39 @@
 import { CalculatorService } from "./calculator.service";
-import { LoggerService } from "./logger.service";
 
 describe('CalculatorService', ()=> {
+
+    let calculator: CalculatorService,
+    loggerSpy: any
+
+    beforeEach(()=>{
+        
+        console.log("BeforeEach")
+
+        //Spy
+        loggerSpy = jasmine.createSpyObj('logger', ['log']);
+        calculator = new CalculatorService(loggerSpy);
+    })    
     
     it('should add two numbers', ()=>{
 
-        //Spy
-        const logger = jasmine.createSpyObj('logger', ['log']);
+        console.log("Add Test")
 
-        const calculator = new CalculatorService(logger);
         const result = calculator.add(2, 2);
 
-        expect(logger.log).toHaveBeenCalledTimes(1);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
         expect(result).toBe(4);
 
     })
 
     it('should substract two numbers', ()=>{
 
-        const calculator = new CalculatorService(new LoggerService);
+        console.log("Sub Test")
+
+
         const result = calculator.subtract(2, 2);
 
         expect(result).toBe(0);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
 
     })
 });
